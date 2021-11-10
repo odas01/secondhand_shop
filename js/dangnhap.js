63947465e1651146c236daaec7fcccbeb6184983
TienThanh <lntthanh3317@gmail.com>
2021-11-10 12:28:42 +0700

    edit login form

---
+++ js/dangnhap.js
@@ -0,0 +1,82 @@
+function Validator(selector) {
+   // Quy tắc
+   var validatorRules = {
+      required: function (value) {
+         return value.trim() ? undefined : 'Vui lòng nhập trường này';
+      },
+
+      minLength: function (min) {
+         return function (value) {
+            return value.length >= min
+               ? undefined
+               : `Vui lòng nhập ít nhất ${min} ký tự`;
+         };
+      },
+   };
+
+   //Hàm lấy ra thẻ cha chứ form-error
+   function getParent(inputElement, nameForm) {
+      while (inputElement.parentElement) {
+         if (inputElement.parentElement.matches(nameForm)) {
+            return inputElement.parentElement;
+         }
+         inputElement = inputElement.parentElement;
+      }
+   }
+
+   //Chứa rule
+   var formRules = {};
+   //Lấy ra form
+   var formElement = document.querySelector(selector);
+   if (formElement) {
+      var inputs = formElement.querySelectorAll('[name][rules]');
+      for (var input of inputs) {
+         var rules = input.getAttribute('rules').split('|');
+         for (var rule of rules) {
+            var ruleHasValue = rule.includes(':');
+            if (ruleHasValue) {
+               var ruleInfo = rule.split(':');
+               rule = ruleInfo[0];
+            }
+            var ruleFunc = validatorRules[rule];
+            if (ruleHasValue) {
+               ruleFunc = validatorRules[rule](ruleInfo[1]);
+            }
+
+            if (!Array.isArray(formRules[input.name])) {
+               formRules[input.name] = [ruleFunc];
+            } else {
+               formRules[input.name].push(ruleFunc);
+            }
+         }
+         input.onblur = validate;
+         input.oninput = clearError;
+      }
+   }
+
+   function validate(e) {
+      var rules = formRules[e.target.name];
+      var errorMessage;
+      for (var rule of rules) {
+         if (errorMessage) break;
+         errorMessage = rule(e.target.value);
+      }
+
+      var parent = getParent(e.target, '.form-group');
+      var errorElement = parent.querySelector('.form-error');
+      if (errorMessage) {
+         errorElement.innerHTML = errorMessage;
+         parent.classList.add('invalid');
+      } else {
+         errorElement.innerHTML = '';
+         parent.classList.remove('invalid');
+      }
+   }
+
+   function clearError(e) {
+      var parent = getParent(e.target, '.form-group');
+      var errorElement = parent.querySelector('.form-error');
+      errorElement.innerHTML = '';
+      parent.classList.remove('invalid');
+   }
+}
